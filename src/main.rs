b7c958b2ff0af1cd3264a9a2e52d2069231473c2
Swastik <swastik.patro02@gmail.com>
2026-06-18 16:42:50 +0530

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,53 @@
 use micrograd_rs::{Value, micrograd::nn::MLP};
 
 fn main() {
-
-    // this below is a dummy dataset from karpathy's video 
+    // this below is a dummy dataset from karpathy's video
     let xs = vec![
         vec![Value::new(2.0), Value::new(3.0), Value::new(-1.0)],
         vec![Value::new(3.0), Value::new(-1.0), Value::new(0.5)],
         vec![Value::new(0.5), Value::new(1.0), Value::new(1.0)],
         vec![Value::new(1.0), Value::new(1.0), Value::new(-1.0)],
     ];
-    let ys = vec![Value::new(1.0),Value::new(-1.0),Value::new(-1.0),Value::new(1.0)];
+    let ys = vec![
+        Value::new(1.0),
+        Value::new(-1.0),
+        Value::new(-1.0),
+        Value::new(1.0),
+    ];
 
-    let mlp = MLP::new(&[4,4,1], 3); // nn with 3 inputs, two hidden layers with 4 neurons, 1 output
-    let epochs = 20;
+    let mlp = MLP::new(&[4, 4, 1], 3); // nn with 3 imputs , two hiddenlayes with 4 neurons, 1 output
+    let epochs = 30;
     let lr = 0.04;
 
-    for epoch in 0..epochs{
-        // FORWARD PASS
+    for epoch in 0..epochs {
         let mut y_nn = Vec::new();
-        for x in &xs{
-            let pred = mlp.forward_pass(x)[0].clone();
+        for x in &xs {
+            let pred = mlp.forward_pass(x)[0].clone(); // we jsut have one output neuron
             y_nn.push(pred);
         }
 
-        // LOSS (MSE)
         let mut loss = Value::new(0.0);
-        for (y_pred, y) in y_nn.iter().zip(ys.iter()){
-            let diff = y_pred.clone() - y.clone();
+        for (y_pred, y) in y_nn.iter().zip(ys.iter()) {
+            let diff = y_pred.clone() - y.clone(); //jsut finding the loss
             let diff_sq = diff.clone() * diff.clone();
             loss = loss + diff_sq;
         }
 
-        // ZERO GRADIENT
-        for param in mlp.parameters(){
+        //ZERO GRADIENT
+        for param in mlp.parameters() {
             param.zero_grad();
         }
 
-        // BACKWARD
         loss.backward();
 
-        // UPDATE WEIGHTS
-        for param in mlp.parameters(){
+        for param in mlp.parameters() {
+            //gonna update every single parameter
             let grad = param.grad();
-            let updated = param.data() - lr * grad;
-            param.set_data(updated);
+            //param.data() -= lr * grad; ERROR as we need something to into RefCell and change this fucntion call is not actually changin that value
+            let update = param.data() - lr * grad;
+            param.set_data(update);
         }
 
-        println!("epoch {:2} | loss: {:.6}", epoch, loss.data());
+        println!("Epoch {}: Loss = {}", epoch, loss.data());
     }
-    
 }
